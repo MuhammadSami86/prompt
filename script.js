1ad4dbde47b8177ca3e0fe640d235089f503c237
“muhammad <muhammadsamik681@gmail.com>
2024-10-30 11:57:05 +0500

    origin main

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// var city = prompt("which one city of light")
+// if(city === "karachi"){
+//     alert("welcome to city of light")
+//     console.log("welcome to city of" + "  "+city);
+    
+// }else{
+//     alert("please correc city")
+//     console.log("your answer incorrect"+"  "+city);
+    
+// }
+
+var gender = prompt("Entr your gender male & female")
+if(gender === "male"){
+    alert("welcome to sir")
+}
+else if(gender === "female"){
+ alert("welcome to madam")
+}else{
+    alert("please enter your correct gender")
+}
